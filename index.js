a1eef5a2173abaad09cc845a59b8aa2a05f17647
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-01-30 23:50:17 +0300

    middleweraa upload

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+	console.log('middleweare 1');
+	next();
+});
+app.use((req, res, next) => {
+	console.log('middleweare 2');
+	res.send('<h1>salomlar</h1>');
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {

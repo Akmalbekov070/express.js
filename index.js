e51f32b13e79e892d67932630383080a72adbefb
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-01-31 21:12:32 +0300

    request pars qilish

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/', (req, res, next) => {
-	console.log('bu har doim ishlaydi');
 	next();
 });
+app.use('/New-users', (req, res, next) => {
+	res.send(`
+     <form action="/users" method="POST">
+     <input type="text" name="username" >
+     <input type="number" name="age">
+     <button>send</button>
+     </form> 
+  `);
+});
+
 app.use('/users', (req, res, next) => {
-	res.send('<h1>Keyingi sahifa</h1>');
+	console.log(req.body.username);
+	console.log(req.body.age);
+	res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {

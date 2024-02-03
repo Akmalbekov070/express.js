edc403e8a7ebbf54b1d7683ee6ac53421158f8ce
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-02-03 22:31:01 +0300

    tuning user page

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get('/new-add', (req, res) => {
+router.get('/added-user.html', (req, res) => {
 	res.sendFile(path.join(__dirname, '..', 'views', 'added-user.html'));
 });
 
 router.post('/users', (req, res) => {
 	console.log(req.body);
+	res.send(req.body);
 	res.redirect('/');
 });
 

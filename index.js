d0ac5301219a941068556965af8cd889426b6ee2
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-02-05 22:13:05 +0300

    static page css

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const useRoutes = require('./routes/user');
 const useMain = require('./routes/main');

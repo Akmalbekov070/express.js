d31139683f2f03b088136341a2755e688d6680e9
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-02-09 16:44:15 +0300

    error commit

--- routes/main.js
+++ routes/main.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-const { users } = require('./user');
+const users = require('./user');
 
 router.get('/', (req, res) => {
+	console.log(users);
 	res.render('main', {
 		title: 'user list',
 		users,

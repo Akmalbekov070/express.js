e1e93e1b391a8eb3252132c8ebeca8760cd946fa
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-02-12 22:01:36 +0300

    an error was found

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const app = express();
 
 // app.set('view engine', 'pug');
 app.engine('handlebars', engine());
-app.set('views engine', 'handlebars');
-
+app.set('view engine', 'handlebars');
 app.set('views', 'views');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,7 +17,7 @@ app.use(useRoutes.router);
 app.use(useMain);
 
 app.use((req, res) => {
-	res.status(404).sendFile(path.join(__dirname, 'views', 'notFound.html'));
+	res.status(404).render('404', { title: 'page not found' });
 });
 
 const PORT = process.env.PORT || 3000;

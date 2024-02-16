2d935cb7ff335a1fe1a3cace5994d7b5ba5f159c
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-02-16 22:01:17 +0300

    templede lesson finish

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 // const { engine } = require('express-handlebars');
 const app = express();
-
+//pug=======================
 // app.set('view engine', 'pug');
+//handlebars===========
 //app.engine('handlebars', engine());
 //app.set('view engine', 'handlebars');
+//ejs=============
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.json());
@@ -26,3 +28,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log('port has benn start');
 });
+
+//shablanizator

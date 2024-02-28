54db0d5a763d4b19fc570f71fd9b7532b96312ea
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-02-29 02:20:58 +0300

    path user_info

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,24 @@
 const path = require('path');
-const User = require('../models/users');
+const pool = require('../config/db');
+const { log } = require('console');
 
 const AddedPage = (req, res) => {
 	res.sendFile(path.join(__dirname, '..', 'views', 'added-user.html'));
 };
 
-const UsersPage = (req, res) => {
-	const users = new User(req.body.username, req.body.age);
-	users.save();
-	res.redirect('/');
+const UsersPage = async (req, res) => {
+	try {
+		const newUsers = await pool.query(
+			`
+		INSERT INTO user_info (username, age) VALUES ($1,$2)
+		`,
+			[req.body.username, req.body.age]
+		);
+		res.redirect('/');
+		console.log(newUsers);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 module.exports = {

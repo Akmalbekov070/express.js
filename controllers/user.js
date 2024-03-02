15c9575a6d88788b443abf46f594ac742a0d9c73
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-03-02 22:42:24 +0300

    copy uptedUser

--- controllers/user.js
+++ controllers/user.js
@@ -32,9 +32,21 @@ const UptedUserPage = async (req, res) => {
 		console.log(error);
 	}
 };
+const UptedUser = async (req, res) => {
+	try {
+		const user = await pool.query('SELECT * FROM user_info WHERE id = $1', [req.params.id]);
+		res.render('edit-user', {
+			title: 'edit-user',
+			user: user.rows[0],
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 module.exports = {
 	AddedPage,
 	UsersPage,
 	UptedUserPage,
+	UptedUser,
 };

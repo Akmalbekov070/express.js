d0b94e2be3a164a25716cfd35365e65b68977d45
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-03-04 19:03:38 +0300

    button page upload

--- controllers/main.js
+++ controllers/main.js
@@ -4,7 +4,6 @@ const User = require('../models/users');
 const MainPage = async (req, res) => {
 	try {
 		const users = await pool.query('SELECT * FROM user_info');
-		console.log(users);
 		res.render('main', {
 			title: 'User list',
 			users: users.rows,
